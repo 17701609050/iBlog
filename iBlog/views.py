34f93aefaa81067bcae971c0589982f095601f1f
zipinglx <zipingx.lv@intel.com>
2018-07-06 16:33:03 +0800

    fix some bug

--- iBlog/views.py
+++ iBlog/views.py
@@ -7,29 +7,29 @@ from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render_to_response
 from django.http.response import HttpResponseRedirect, HttpResponse
 from django.template import TemplateDoesNotExist, RequestContext
-from .login_service import do_login
+# from .login_service import do_login
 
-@csrf_exempt
-def user_login(request):
-    if request.method == 'GET':
-        return render_to_response('common/login.html',  {}, RequestContext(request))
-    else:
-        res = {'result': 'error'}
-        response = HttpResponse()
-        try:
-            if request.POST.get('username') is not None:
-                (result, s,) = do_login(request, True)
-                res.update({'result': ('success' if result else 'fail')})
-        except Exception as e:
-            raise e
-        finally:
-            res.update({'user_token': request.session.session_key})
-            response.write(json.dumps(res).encode('utf-8'))
-            return response
-
-@csrf_exempt
-def user_logout(request):
-    # do something for logout
-    logout(request)
-    return render_to_response("common/logout.html", {}, RequestContext(request))
+# @csrf_exempt
+# def user_login(request):
+#     if request.method == 'GET':
+#         return render_to_response('common/login.html',  {}, RequestContext(request))
+#     else:
+#         res = {'result': 'error'}
+#         response = HttpResponse()
+#         try:
+#             if request.POST.get('username') is not None:
+#                 (result, s,) = do_login(request, True)
+#                 res.update({'result': ('success' if result else 'fail')})
+#         except Exception as e:
+#             raise e
+#         finally:
+#             res.update({'user_token': request.session.session_key})
+#             response.write(json.dumps(res).encode('utf-8'))
+#             return response
+#
+# @csrf_exempt
+# def user_logout(request):
+#     # do something for logout
+#     logout(request)
+#     return render_to_response("common/logout.html", {}, RequestContext(request))
 
