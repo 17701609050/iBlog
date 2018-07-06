e8449de82814be122ff7f9ad416ab4c5bde1d62d
zipinglx <zipingx.lv@intel.com>
2018-07-06 16:40:35 +0800

    fix some bug

--- iBlog/settings.py
+++ iBlog/settings.py
@@ -86,7 +86,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        # 'APP_DIRS': True,
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
