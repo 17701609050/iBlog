3b65794f73d6f08cd496820d9f385d5dd0446d99
zipinglv <459260276@qq.com>
2018-07-07 21:43:11 +0800

    add blog search

--- apps/blog/search_blogs.py
+++ apps/blog/search_blogs.py
@@ -5,6 +5,7 @@
 # @Email   : 17701609050@163.com
 # -----------------------------------------------------
 import requests
+import urllib2
 from pyquery import PyQuery as pq
 import sys
 reload(sys)
@@ -14,6 +15,7 @@ class FindBlogs(object):
 
     def __init__(self):
         self.url_index = 'http://zzk-s.cnblogs.com/s/blogpost?Keywords={0}&ViewCount=200&pageindex={1}'
+        self.proxys = {"http": "139.224.80.139:3128"}
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0",
             "Cookie": ";"
@@ -21,7 +23,7 @@ class FindBlogs(object):
         self.search_content = []
 
     def search_blogs(self, searchkey, pageindex="1"):
-        result = requests.get(self.url_index.format(searchkey, pageindex), headers=self.headers)
+        result = requests.get(self.url_index.format(searchkey, pageindex), headers=self.headers, proxies=self.proxys)
         print result.status_code
         if result.status_code == 200:
             self.search_content = result.content.encode('UTF-8')
@@ -51,8 +53,41 @@ searchblog = FindBlogs()
 
 if __name__ == "__main__":
     blog = FindBlogs()
-    search_result = blog.search_blogs("celery实例")
-    # blogs = blog.parse_html(search_result)
+    search_result = blog.search_blogs("celery")
+
+
+
+    # import urllib2
+    # import random
+    #
+    # def url_user_agent(url):
+    #     # 设置使用代理
+    #     proxy_list = [
+    #         {"http": "139.224.80.139"},
+    #
+    #     ]
+    #     proxy = random.choice(proxy_list)
+    #     # proxy = {'http': '124.88.67.81:80'}
+    #     proxy_support = urllib2.ProxyHandler(proxy)
+    #     # opener = urllib2.build_opener(proxy_support,urllib2.HTTPHandler(debuglevel=1))
+    #     opener = urllib2.build_opener(proxy_support)
+    #     urllib2.install_opener(opener)
+    #
+    #     # 添加头信息，模仿浏览器抓取网页，对付返回403禁止访问的问题
+    #     # i_headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
+    #     i_headers = {
+    #         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.48'}
+    #     req = urllib2.Request(url, headers=i_headers)
+    #     html = urllib2.urlopen(req)
+    #     if url == html.geturl():
+    #         doc = html.read()
+    #         return doc
+    #     return
+    #
+    #
+    # url = 'http://baidu.com'
+    # doc = url_user_agent(url)
+    # print doc
 
 
 
